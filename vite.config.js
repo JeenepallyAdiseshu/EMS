149331b7aac5961c2defa14562461f3ef4fa2dab
Adiseshu <jeenepallyadiseshu@gmail.com>
2025-09-06 00:17:42 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // backend server
+      '/api': 'http://localhost:5000', 
+     base: 'employee',
     },
   },
 })
